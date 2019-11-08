dd443fd9c00e6ddbc6adcacc54f38376125bd245
BenebSiny <stu995106@hotmail.com.tw>
2019-11-09 00:23:36 +0800

    利用JS使螢幕寬度縮小時能將nav裡的item隱藏起來 & 引入fontawesome

---
+++ src/web/js/navigation.js
@@ -0,0 +1,13 @@
+$(window).resize(function () {
+    const width = $(window).width();
+    const navList = $(`nav ul`);
+
+    if (width <= 700) {
+        navList.find(`li`).not(`:has(> button)`).hide();
+        navList.find(`li:has(> button#nav-menu)`).show()
+    }
+    else {
+        navList.find(`li`).not(`:has(> button)`).show();
+        navList.find(`li:has(> button#nav-menu)`).hide()
+    }
+})
