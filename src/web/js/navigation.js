57a29def50d20aa8a0c3f625e5ef19e8226b24b4
BenebSiny <stu995106@hotmail.com.tw>
2019-11-09 02:19:39 +0000

    fix eslint style

--- src/web/js/navigation.js
+++ src/web/js/navigation.js
@@ -1,13 +1,13 @@
 $(window).resize(function () {
-    const width = $(window).width();
-    const navList = $(`nav ul`);
+  const width = $(window).width();
+  const navList = $('nav ul');
 
-    if (width <= 700) {
-        navList.find(`li`).not(`:has(> button)`).hide();
-        navList.find(`li:has(> button#nav-menu)`).show()
-    }
-    else {
-        navList.find(`li`).not(`:has(> button)`).show();
-        navList.find(`li:has(> button#nav-menu)`).hide()
-    }
+  if (width <= 700) {
+    navList.find('li').not(':has(> button)').hide();
+    navList.find('li:has(> button#nav-menu)').show();
+  }
+  else {
+    navList.find('li').not(':has(> button)').show();
+    navList.find('li:has(> button#nav-menu)').hide();
+  }
 })
