1c1c0a4dad0e38e88bccf51b1d6d44777085435c
BenebSiny <stu995106@hotmail.com.tw>
2019-11-09 16:54:13 +0800

    利用CSS來解決螢幕變小時nav會有不一樣的外觀

--- src/web/js/navigation.js
+++ src/web/js/navigation.js
@@ -1,17 +1,3 @@
-$(window).resize(() => {
-  const width = $(window).width();
-  const navList = $('nav#horizontal ul');
-
-  if (width <= 700) {
-    navList.find('li').not(':has(> button)').hide();
-    navList.find('li:has(> button#show-nav-btn)').show();
-  } else {
-    navList.find('li').not(':has(> button)').show();
-    navList.find('li:has(> button#show-nav-btn)').hide();
-    $('nav#vertical').hide();
-  }
-});
-
-$('#show-nav-btn').on('click', () => {
-  $('nav#vertical').toggle();
+$('#show-nav-btn').on('click blur', () => {
+  $('#vertical-nav').toggle();
 })
