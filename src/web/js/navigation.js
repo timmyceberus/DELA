ad086bea65634bed9b4c2098d232985294bb9f55
BenebSiny <stu995106@hotmail.com.tw>
2019-11-10 10:43:05 +0800

    標題隨滑鼠做出相對應的移動

--- src/web/js/navigation.js
+++ src/web/js/navigation.js
@@ -1,3 +1,26 @@
 $('#show-nav-btn').on('click blur', () => {
   $('#vertical-nav').toggle();
 });
+
+const mousePos = { x: -1, y: -1 };
+const relativePos = { x: -1, y: -1 };
+const titleObject = $('#title div');
+const titlePos = {
+  x: titleObject.offset().left + titleObject.width() / 2,
+  y: titleObject.offset().top + titleObject.height() / 2,
+};
+
+
+$(document).on('mousemove', (event) => {
+  mousePos.x = event.pageX;
+  mousePos.y = event.pageY;
+
+  relativePos.x = titlePos.x - mousePos.x;
+  relativePos.y = titlePos.y - mousePos.y;
+
+  titleObject.css({
+    left: `${titlePos.x + relativePos.x * 0.02 - titleObject.width() / 2}px`,
+    top: `${titlePos.y + relativePos.y * 0.02 - titleObject.height() / 2}px`,
+  });
+  // console.log(mouse, title, relative);
+});
