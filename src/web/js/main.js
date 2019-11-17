17ce834f30f1e792fcc260f4a24c00c2dda4b539
BenebSiny <stu995106@hotmail.com.tw>
2019-11-17 12:02:31 +0800

    fixed title position
    for those teammates who thought floating title was a terrible idea

--- src/web/js/main.js
+++ src/web/js/main.js
@@ -66,35 +66,3 @@ $(window).on('scroll', () => {
 if ($(window).scrollTop() > $('#title').height() - 100) {
   changeNavBGColor();
 }
-
-// TITLE
-let sitewidth = $(window).width();
-const mousePos = { x: -1, y: -1 };
-const relativePos = { x: -1, y: -1 };
-const bgobject = $('#title');
-const titleobject = $('#title div');
-const titlePos = {
-  x: sitewidth / 2,
-  y: titleobject.offset().top + titleobject.height() / 2,
-};
-
-$(document).on('mousemove', (event) => {
-  mousePos.x = event.pageX;
-  mousePos.y = event.pageY;
-
-  relativePos.x = titlePos.x - mousePos.x;
-  relativePos.y = titlePos.y - mousePos.y;
-
-  titleobject.css({
-    left: `${((titlePos.x + relativePos.x * 0.03) / sitewidth) * 100}vw`,
-    top: `${(titlePos.y + relativePos.y * 0.03)}px`,
-  });
-
-  bgobject.css('background-position', `${50 + relativePos.x * 0.001}% ${50 + relativePos.y * 0.01}%`);
-});
-
-$(window).resize(() => {
-  sitewidth = $(window).width();
-  titlePos.x = sitewidth / 2;
-  titlePos.y = titleobject.offset().top + titleobject.height() / 2;
-});
