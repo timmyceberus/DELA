c5f0ac11585c9849d40df9ccf72e4cbc91b10096
鄭志軒 <stu995106@hotmail.com.tw>
2019-12-04 17:58:12 +0800

    登入validate & 錯誤變色

---
+++ src/web/js/login.js
@@ -0,0 +1,18 @@
+$('button[type=submit]').on('click', () => {
+    const name = $('input[type=e-mail]').val();
+    const password = $('input[type=password').val();
+
+    if(name!='' && password!='') {
+        localStorage.setItem('name', name);
+        window.location.href = "index.html"
+    }
+    else if(password==='') {
+        $('input[type=password] ~ span.float').addClass('error');
+        $('input[type=password] ~ span.faded').addClass('error');
+    }
+});
+
+$('input[type=password]').on('keydown', ()=>{
+    $('input[type=password] ~ span.float').removeClass('error');
+    $('input[type=password] ~ span.faded').removeClass('error');
+})
