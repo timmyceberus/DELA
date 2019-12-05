0cd3479c323ab0a7a0b4786040ff6706ced80941
鄭志軒 <stu995106@hotmail.com.tw>
2019-12-05 14:58:44 +0800

    enter鍵提交帳號密碼

--- src/web/js/login.js
+++ src/web/js/login.js
@@ -1,34 +1,41 @@
-$('button[type=submit]').on('click', () => {
-    const name = $('input[type=text]').val();
-    const password = $('input[type=password').val();
+function submit() {
+  const name = $('input[type=text]').val();
+  const password = $('input[type=password').val();
 
-    if(name!='' && password!='') {
-        localStorage.setItem('name', name);
-        window.location.href = "index.html"
-    }
+  if (name != '' && password != '') {
+    localStorage.setItem('name', name);
+    window.location.href = "index.html"
+  }
 
-    if (name==='') {
-        $('input[type=text] ~ span.float').addClass('error');
-        $('input[type=text] ~ span.faded').addClass('error');
-        $('input[type=text] ~ .error-message').addClass('show');
-    }
+  if (name === '') {
+    $('input[type=text] ~ span.float').addClass('error');
+    $('input[type=text] ~ span.faded').addClass('error');
+    $('input[type=text] ~ .error-message').addClass('show');
+  }
 
-    if (password==='') {
-        $('input[type=password] ~ span.float').addClass('error');
-        $('input[type=password] ~ span.faded').addClass('error');
-        $('input[type=password] ~ .error-message').addClass('show');
-    }
-});
+  if (password === '') {
+    $('input[type=password] ~ span.float').addClass('error');
+    $('input[type=password] ~ span.faded').addClass('error');
+    $('input[type=password] ~ .error-message').addClass('show');
+  }
+}
 
-$('input[type=text]').on('keydown', ()=>{
-    $('input[type=text] ~ span.float').removeClass('error');
-    $('input[type=text] ~ span.faded').removeClass('error');
-    $('input[type=text] ~ .error-message').removeClass('show');
+$('button[type=submit]').on('click', submit);
 
+$('input[type=text]').on('keydown', () => {
+  $('input[type=text] ~ span.float').removeClass('error');
+  $('input[type=text] ~ span.faded').removeClass('error');
+  $('input[type=text] ~ .error-message').removeClass('show');
 })
 
-$('input[type=password]').on('keydown', ()=>{
-    $('input[type=password] ~ span.float').removeClass('error');
-    $('input[type=password] ~ span.faded').removeClass('error');
-    $('input[type=password] ~ .error-message').removeClass('show');
+$('input[type=password]').on('keydown', () => {
+  $('input[type=password] ~ span.float').removeClass('error');
+  $('input[type=password] ~ span.faded').removeClass('error');
+  $('input[type=password] ~ .error-message').removeClass('show');
+})
+
+$('input[type=text], input[type=password]').on('keypress', (event) => {
+  if (event.which == 13) {
+    submit();
+  }
 })
