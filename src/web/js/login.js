10d3958a5c1c90f9838a4920626b042b7774c8c5
鄭志軒 <stu995106@hotmail.com.tw>
2019-12-05 14:40:01 +0800

    輸入錯誤的提示

--- src/web/js/login.js
+++ src/web/js/login.js
@@ -1,5 +1,5 @@
 $('button[type=submit]').on('click', () => {
-    const name = $('input[type=e-mail]').val();
+    const name = $('input[type=text]').val();
     const password = $('input[type=password').val();
 
     if(name!='' && password!='') {
@@ -8,22 +8,27 @@ $('button[type=submit]').on('click', () => {
     }
 
     if (name==='') {
-        $('input[type=e-mail] ~ span.float').addClass('error');
-        $('input[type=e-mail] ~ span.faded').addClass('error');
+        $('input[type=text] ~ span.float').addClass('error');
+        $('input[type=text] ~ span.faded').addClass('error');
+        $('input[type=text] ~ .error-message').addClass('show');
     }
 
     if (password==='') {
         $('input[type=password] ~ span.float').addClass('error');
         $('input[type=password] ~ span.faded').addClass('error');
+        $('input[type=password] ~ .error-message').addClass('show');
     }
 });
+
+$('input[type=text]').on('keydown', ()=>{
+    $('input[type=text] ~ span.float').removeClass('error');
+    $('input[type=text] ~ span.faded').removeClass('error');
+    $('input[type=text] ~ .error-message').removeClass('show');
+
+})
 
 $('input[type=password]').on('keydown', ()=>{
     $('input[type=password] ~ span.float').removeClass('error');
     $('input[type=password] ~ span.faded').removeClass('error');
-})
-
-$('input[type=e-mail]').on('keydown', ()=>{
-    $('input[type=e-mail] ~ span.float').removeClass('error');
-    $('input[type=e-mail] ~ span.faded').removeClass('error');
+    $('input[type=password] ~ .error-message').removeClass('show');
 })
